9317164946eed60805778ab5a4ca3cb8fe0537d7
yelsinkiprop <yelsin.kiprop@student.moringaschool.com>
2025-07-15 21:55:31 +0300

    all comments

---
+++ server/models/bus.py
@@ -0,0 +1,11 @@
+from . import db
+
+class Bus(db.Model):
+    __tablename__ = 'buses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    routeid = db.Column(db.Integer, db.ForeignKey('routes.id'))
+    numberplate = db.Column(db.String(50))
+    capacity = db.Column(db.Integer)
+    route = db.relationship('Route', back_populates='buses')
+    bookings = db.relationship('Booking', back_populates='bus')
