5d6fb2ebffb1680a16e166e9b2c27977b97b95b4
Brian <briankagia24@gmail.com>
2025-07-16 10:03:51 +0300

    Add initial admin page layout

--- client/app/login/page.jsx
+++ client/app/login/page.jsx
@@ -1,66 +1,67 @@
-'use client';
+"use client";
 
-import {useState} from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-export default function LoginPage(){
-    const Router= useRouter();
-    const [email, setEmail]=useState("");
-    const[password, setPassword]=useState("");
+export default function LoginPage() {
+  const Router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleLogin = async (e) =>{
-        e.preventDefault();
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-        try {
-            const res= await fetch("http://localhost:5000/login",{
-                method:"POST",
-                headers:{"Content-Type":"application/json"},
-                body: JSON.stringify({email, password}),
-            });
+    try {
+      const res = await fetch("http://localhost:5000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
 
-            const data= await res.json();
+      const data = await res.json();
 
-            if (res.ok && data?.user){
-                localStorage.setItem("token", data.user);
-                localStorage.setItem("user", JSON.stringify(data.user));
+      if (res.ok && data?.user) {
+        localStorage.setItem("token", data.user);
+        localStorage.setItem("user", JSON.stringify(data.user));
 
-                const role= data.user.role;
-                Router.push(role==="admin" ? "/admin/dashboard": "/book-seat");
-            }
-            else {
-                alert (data.error || "Login Failed. Please check your credentials and try again"
-            )}
-        } catch (err){
-            console.error("Login error:", err);
-            alert("Server error");
-        }
-    };
+        const role = data.user.role;
+        Router.push(role === "admin" ? "/admin/dashboard" : "/book-seat");
+      } else {
+        alert(
+          data.error ||
+            "Login Failed. Please check your credentials and try again"
+        );
+      }
+    } catch (err) {
+      console.error("Login error:", err);
+      alert("Server error");
+    }
+  };
 
-    return (
-         <main className="p-4">
-          <h1 className="text-xl font-bold mb-4">Login</h1>
-          <form onSubmit={handleLogin} className="flex flex-col gap-4 max-w-md">
-            <input
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="border p-2 rounded"
-            required
-            />
-            <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="border p-2 rounded"
-            required
-           />
-           <button type="submit" className="bg-green-600 text-white p-2 rounded">
-            Login
-           </button>
-          </form>
-         </main>
-         
-    )
-};
+  return (
+    <main className="p-4">
+      <h1 className="text-xl font-bold mb-4">Login</h1>
+      <form onSubmit={handleLogin} className="flex flex-col gap-4 max-w-md">
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border p-2 rounded"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border p-2 rounded"
+          required
+        />
+        <button type="submit" className="bg-green-600 text-white p-2 rounded">
+          Login
+        </button>
+      </form>
+    </main>
+  );
+}
