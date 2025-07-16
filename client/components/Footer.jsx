0f807c89555ddf83889a14b8c5cc7f2fd29e4da5
Rosemary Kamau <kamaurosemary.n@gmail.com>
2025-07-16 15:53:08 +0300

    Add add-footer

---
+++ client/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 text-center text-gray-500 text-sm py-4 mt-auto">
+      &copy; {new Date().getFullYear()} School Minibus Booking. All rights reserved.
+    </footer>
+  );
+}
