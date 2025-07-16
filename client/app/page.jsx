67b6532ce35d0238640a8dc1c11b791a1e2b3886
Mellon Obada <mellobadah123@gmail.com>
2025-07-16 16:26:00 +0300

    worked on hero section, changed login logic and added a photo in public

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,8 +1,10 @@
+import HeroSection from "../components/HeroSection"
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Welcome to the School Minibus Booking System</h1>
-      <p>Book your seat for your child quickly and easily.</p>
+      <HeroSection />
     </div>
   );
 }
+
