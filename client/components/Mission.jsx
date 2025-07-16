577d17378d1d128897b3a6a7e2b8ef60fb7afbcb
Brian <briankagia24@gmail.com>
2025-07-17 00:11:03 +0300

    Adds About us section

---
+++ client/components/Mission.jsx
@@ -0,0 +1,12 @@
+export default function Mission() {
+    return (
+      <section className="bg-gray-100 py-12">
+        <div className="max-w-4xl mx-auto px-4 text-center shadow-md rounded-xl">
+          <h2 className="text-3xl font-bold mb-4">Our Mission</h2>
+          <p className="text-gray-700 text-lg">
+            To revolutionize school transportation across the region by offering a smart, accessible, and dependable solution that prioritizes children’s safety, parents’ peace of mind, and community development.
+          </p>
+        </div>
+      </section>
+    )
+  }
