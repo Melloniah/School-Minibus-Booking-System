08cb70dc1fda735489289e4e7faaec26b77d7ac6
Elsie <elsieoscar06@gmail.com>
2025-07-16 14:00:40 +0300

    pushing the booking api end points

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -6,4 +6,4 @@ from .user import User
 from .route import Route
 from .pickup_dropoff_location import Pickup_Dropoff_Location  
 from .bus import Bus 
-from .booking import Booking 
+from .Booking import Booking 
