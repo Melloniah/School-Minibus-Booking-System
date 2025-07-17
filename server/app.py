1f49c09346d82df0964747b27338e00e6f40cf47
Joan Nyawira Kori <korijoan171@gmail.com>
2025-07-17 15:13:48 +0300

    Added login & cookies route, jwt in bookingController,Removed werkzeug from users table

--- server/app.py
+++ server/app.py
@@ -4,12 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 from flask_cors import CORS 
 from flask_restful import Api
-
-# we will import from routes eg(from routes.auth import auth_bp)
+from flask_jwt_extended import JWTManager
 
 from routes.booking_route import booking_bp
 # import booking management logic(user bookings,canceling,etc)
-from routes.auth_route import registration_bp
+from routes.auth_route import registration_bp, login_bp
+
+# JWT Config
+app.config['JWT_SECRET_KEY'] = 'your-secret-key'
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token_cookie'
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False  
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///minibus.db'
@@ -20,6 +25,7 @@ migrate = Migrate(app, db)
 CORS(app)
 
 api = Api(app)
+jwt = JWTManager(app)
 
 # register blueprints eg(app.register_blueprint(auth_bp, url_prefix='/auth'))
 #Instead of writing all routes in app.py, we group related routes in separate files 
@@ -27,8 +33,9 @@ api = Api(app)
 
 app.register_blueprint(booking_bp, url_prefix="/bookings")
 app.register_blueprint(registration_bp, url_prefix="/register")
+app.register_blueprint(login_bp, url_prefix="/login")
 
 # This is where you tell Flask to include those route groups into the main app.
 # blueprint is booking_bp the example final route are (/bookings, bookings/1)
-# so falsk knows when I see a request atarting with /auth , go use the auth_bp
+# so flask knows when I see a request atarting with /auth , go use the auth_bp
 
