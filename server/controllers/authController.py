121480ebfb249c132770bfc656c03eee03f3f9d1
Joan Nyawira Kori <korijoan171@gmail.com>
2025-07-18 02:08:47 +0300

    Modified authController,tested register and login routes(functioning well)

--- server/controllers/authController.py
+++ server/controllers/authController.py
@@ -18,7 +18,7 @@ def register_user():
     if User.query.filter_by(email=email).first():
         return make_response(f"This user already exists", 400)
     
-    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
     new_user = User(email=email, name=name, password=hashed)
     db.session.add(new_user)
     db.session.commit()
@@ -32,9 +32,9 @@ def login():
     email = data["email"]
 
     user = User.query.filter_by(email= email).first()
-    if user and bcrypt.checkpw(password.encode('utf-8'), user.password_hash):
+    if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
         access_token = create_access_token(identity=email) #gerate JWT
-        response = make_response(f"Welcome {user.username}")
+        response = make_response(f"Welcome {user.name}")
         set_access_cookies(response, access_token) # save JWT in httponly cookies
         return response   
          
