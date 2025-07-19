9aa48853f8d03b9c19332c733003d56020abf36a
Joan Nyawira Kori <korijoan171@gmail.com>
2025-07-19 22:28:27 +0300

    Added admin table,login roles(admin and user),tokens generated successfully

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+from .admin import Admin
 from .user import User
 from .route import Route
 from .pickup_dropoff_location import Pickup_Dropoff_Location  
