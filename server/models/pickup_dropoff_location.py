c07b945b544e55c7de57654b08d01239c1b27084
Joan Nyawira Kori <korijoan171@gmail.com>
2025-07-19 22:28:27 +0300

    Added admin table,login roles(admin and user),tokens generated successfully

--- server/models/pickup_dropoff_location.py
+++ server/models/pickup_dropoff_location.py
@@ -7,7 +7,6 @@ class Pickup_Dropoff_Location(db.Model,SerializeMixin):
     id = db.Column(db.Integer,primary_key=True)
     name_location = db.Column(db.String)
     GPSystem = db.Column(db.String)
-
     route_id = db.Column(db.Integer, db.ForeignKey('routes.id'))
 
     route = db.relationship('Route', back_populates='pickup_dropoff_locations')
