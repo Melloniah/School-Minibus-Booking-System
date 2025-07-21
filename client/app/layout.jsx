0179372f8340692e0f7c7327cff86d5a1285ab19
Mellon Obada <mellobadah123@gmail.com>
2025-07-21 22:32:37 +0300

    Setup auth context and update login layout and backend routes

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,13 +1,15 @@
 // client/app/layout.jsx
 
-import { Toaster } from "react-hot-toast";
+// client/app/layout.jsx
 
+import { Toaster } from "react-hot-toast";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'leaflet/dist/leaflet.css';
+
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-
+import AuthProvider from "../context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,10 +33,11 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Toaster position="top-center" />
-
-        <Navbar/>
-        <main>{children}</main>
-        <Footer/>
+        <AuthProvider>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
--- server/routes/auth_route.py
+++ server/routes/auth_route.py
@@ -1,11 +1,13 @@
 # Defines routes like POST /api/auth/register and POST /login.
 from flask import Blueprint
-from controllers.authController import register_user, login, read_cookie
+from controllers.authController import register_user, login, get_current_user, logout
+
 
 # Create blueprints
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 # Register routes
 auth_bp.route('/register', methods=['POST'])(register_user)
 auth_bp.route('/login', methods=['POST'])(login)
-auth_bp.route('/cookie', methods=['GET'])(read_cookie)
+auth_bp.route('/current_user', methods=['GET'])(get_current_user)
+auth_bp.route('/logout', methods=['POST'])(logout)
