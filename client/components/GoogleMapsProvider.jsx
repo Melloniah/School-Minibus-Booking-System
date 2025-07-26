ea530b5875f2458f70356c97e878b0a3f1ee3189
Mellon Obada <mellobadah123@gmail.com>
2025-07-26 17:36:41 +0300

    all unstaged changes

--- client/components/GoogleMapsProvider.jsx
+++ client/components/GoogleMapsProvider.jsx
@@ -4,10 +4,10 @@
 import { LoadScript } from '@react-google-maps/api';
 
 export default function GoogleMapsProvider({ children }) {
-  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
 
   return (
-    <LoadScript googleMapsApiKey={apiKey}>
+    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
       {children}
     </LoadScript>
   );
