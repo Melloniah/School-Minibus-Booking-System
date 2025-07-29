4d6dab91e15c1742fe2145e1cf923788146a0f72
Mellon Obada <mellobadah123@gmail.com>
2025-07-29 14:48:09 +0300

    added route and bus availability known in routecontroller when booking

--- server/routes/routes.py
+++ server/routes/routes.py
@@ -5,17 +5,20 @@ from controllers.routeController import (
     get_routes,
     get_route,
     update_route,
-    delete_route
+    delete_route,
+    get_route_detailed_status
 )
 
 route_bp = Blueprint('routes', __name__)
 
 route_bp.route('/', methods=['POST'])(create_route)
 
-route_bp.route('/', methods=['GET'])(get_routes)
+route_bp.route('/', methods=['GET'])(get_routes) #shows emojis and availability now. 
 
 route_bp.route('/<int:id>', methods=['GET'])(get_route)
 
 route_bp.route('/<int:id>', methods=['PUT'])(update_route)
 
 route_bp.route('/<int:id>', methods=['DELETE'])(delete_route)
+
+route_bp.route('/<int:id>', methods=['GET'])(get_route_detailed_status)
